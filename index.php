12da250badf311bfac56202b9edb294c73329022
ahay0123 <azharrizqillah01@gmail.com>
2024-10-12 09:26:55 +0700

    Templating web apotek

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- Menu -->
+<?php
+    include_once('templates/header.php');
+?>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="block-header">
+            <h2>BLANK PAGE</h2>
+        </div>
+    </div>
+</section>
+
+<?php
+    include_once('templates/footer.php')
+?>
